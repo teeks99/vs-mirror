f10a3161b4f4660e5b2b25ceb6033960922de672
Tom Kent <tom@teeks99.com>
2020-11-28 16:30:24 -0600

    Full run for a single edition

--- mirror.py
+++ mirror.py
@@ -1,11 +1,14 @@
 import urllib.request
 import subprocess
 import os
+import json
+import shutil
 
 versions = {
     "15":"https://aka.ms/vs/15/release/",
     "16":"https://aka.ms/vs/16/release/"
 }
+
 editions = {
     "community": "vs_community.exe",
     "professional": "vs_professional.exe",
@@ -13,15 +16,46 @@ editions = {
     "buildtools": "vs_buildtools.exe"
 }
 
+
 def download(version, edition, local_path, language="en-US"):
     url = versions[version] + editions[edition]
     local_file = "vs_" + edition + "_" + version + ".exe"
     print(f"Downloading {url} to {local_file}")
     urllib.request.urlretrieve(url, filename=local_file)
 
-    cmd = f"{local_file} --layout {local_path} --lang {language} --all --wait"
+    cmd = f"{local_file} --layout {os.path.abspath(local_path)}"
+    cmd += f" --lang {language} --all --wait --passive --quiet"
     print(cmd)
     subprocess.call(cmd)
 
+
+def get_version_num(local_path):
+    j = None
+    with open(os.path.join(local_path, "ChannelManifest.json"), "r") as f:
+        j = json.load(f)
+    version = j["info"]["productDisplayVersion"]
+    return version
+
+
+def rename_version(edition, local_path):
+    version_num = get_version_num(local_path)
+    new_dir = "vs_" + edition + "_" + version_num
+
+    shutil.move(local_path, new_dir)
+    return new_dir
+
+
+def create_zip(dir_name):
+    archive = shutil.make_archive(dir_name, "zip", base_dir=dir_name)
+    return archive
+
+
+def mirror_one(version, edition):
+    temp_path = "vs_" + edition + "_" + version
+    download(version, edition, temp_path)
+    named_path = rename_version(edition, temp_path)
+    create_zip(named_path)
+
+
 if __name__ == "__main__":
-    download("16", "buildtools", os.path.abspath("vs_buildtools_16"))
+    mirror_one("16", "buildtools")
